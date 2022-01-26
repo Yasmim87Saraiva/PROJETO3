aae90ced117199a58da998a43a61abe76729882b
Ysmim <ysaraiva87@gmil.com>
2022-01-26 11:52:06 -0300

    initial Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Variaveis
+p = 0
+I = 0
+#Entrada
+Numero = int(input("Qual numero deseja colocar ? "))
+#Processamento
+if Numero % 2 == 0:
+    p = Numero
+else :
+    I = Numero
+#Saida
+print(p)
+print(I)
